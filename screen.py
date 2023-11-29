dd5768d71ff6ee3f2363f6e7c6d98dff8b163bb8
leobveloso <leonardob.veloso@outlook.com>
2023-11-29 13:30:42 -0300

    Atualização Botão Remover Processo

--- screen.py
+++ screen.py
@@ -142,30 +142,9 @@ def root():
       global process_list
       index_table = process_list.index[['process_id'] == index_process].tolist()
       process_list = process_list.drop(int(index_table))
+      processListingTable()
       endprocess.destroy()
-      '''Se der ruim, apague a partir daqui'''
-      id_label = Label(process_frame, text="ID Processo")
-      id_label.grid(row=0, column=0, padx=15, pady=5)
-      exec_timelabel = Label(process_frame, text="Tempo de Execução")
-      exec_timelabel.grid(row=0, column=1, padx=15, pady=5)
-      init_timelabel = Label(process_frame, text="Tempo de Início")
-      init_timelabel.grid(row=0, column=2, padx=15, pady=5)
-      deadline_label = Label(process_frame, text="Deadline")
-      deadline_label.grid(row=0, column=3, padx=15, pady=5)
-      priority_label = Label(process_frame, text="Prioridade")
-      priority_label.grid(row=0, column=4, padx=15, pady=5)
-      status_label = Label(process_frame, text="Status")
-      status_label.grid(row=0, column=5, padx=15, pady=5)
-
-      process_total = process_list.shape[0]
-      process_index = process_list.shape[1]
-
-      for x in range(process_total):
-        for y in range(process_index):
-          data_temp = Entry(process_frame)
-          data_temp.grid(row=x+1, column=y, padx=5, pady=5)
-          data_temp.insert(END, process_list.iloc[x, y])
-      '''Até aqui'''
+      
     kill_button = Button(endprocess, text="Confirmar", command=confirmKill)
     kill_button.grid(row=2, column=1, padx=10, pady=10)
 
